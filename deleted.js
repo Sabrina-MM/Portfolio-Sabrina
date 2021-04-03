4b29620150dccf20b83fb0032c02776b3be6909a
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2021-04-03 19:53:20 +0100

    new style

--- deleted.js
+++ deleted.js
@@ -1,1 +1,59 @@
-<div></div>;
+<div class="row">
+  <div class="col-3">
+    <div class="col-4">
+      <img src="img/html.png" class="d-fluid icon" alt="HTML" />
+      <p>HTML</p>
+    </div>
+    <div class="col-4">
+      <img src="img/css.png" class="d-fluid icon" alt="CSS" />
+      <p>CSS</p>
+    </div>
+    <div class="col-4">
+      <img src="img/javascript.png" class="d-fluid icon" alt="Responsive" />
+      <p>JavaScript</p>
+    </div>
+  </div>
+  <div class="col-3">
+    <div class="col-4">
+      <img src="img/responsive.png" class="d-fluid icon" alt="Responsive" />
+      <p>Responsive</p>
+    </div>
+    <div class="col-4">
+      <img src="img/seo.png" class="d-fluid icon" alt="SEO" />
+      <p>SEO</p>
+    </div>
+    <div class="col-4">
+      <img src="img/codesandbox.png" class="d-fluid icon" alt="Bootstrap" />
+      <p>Codesandbox</p>
+    </div>
+  </div>
+
+  <div class="col-3">
+    <div class="col-4">
+      <img src="img/editor.png" class="d-fluid icon" alt="Editor" />
+      <p>Editor</p>
+    </div>
+    <div class="col-4">
+      <img src="img/flexbox.png" class="d-fluid icon" alt="flexbox" />
+      <p>Flexbox</p>
+    </div>
+    <div class="col-4">
+      <img src="img/hosting.png" class="d-fluid icon" alt="Hosting" />
+      <p>Netlify</p>
+    </div>
+  </div>
+  <div class="col-3">
+    <div class="col-4">
+      <img src="img/github.png" class="d-fluid icon" alt="GitHub" />
+      <p>GitHub</p>
+    </div>
+    <div class="co-4l">
+      <img src="img/bootstrap.png" class="d-fluid icon" alt="Bootstrap" />
+      <p>Bootstrap</p>
+    </div>
+    <div class="col-4">
+      <img src="img/material-ui.svg" class="d-fluid icon" alt="Bootstrap" />
+      <p>Material-UI</p>
+    </div>
+  </div>
+</div>;
